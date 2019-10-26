b5a3a7c75ecf6dc9ac22436bab03bf4c557dc5b0
Steve-Jones <steve@stevejones.io>
2019-10-26 04:20:18 -0400

    Add zsh font

--- brew.exclude.sh
+++ brew.exclude.sh
@@ -32,6 +32,17 @@ chsh -s /bin/zsh
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+## agnoster.zsh-theme: https://github.com/agnoster/agnoster-zsh-theme
+# Powerline-patched font
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 # ---------------------------------------------
 # Programming Languages and Frameworks
 # ---------------------------------------------
